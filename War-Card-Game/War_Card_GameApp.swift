b4b6d5ac4642a86e9c6c9d8c920bad04428b84a3
Raghuram Duvvuri <raghuramduvvuri12@gmail.com>
2025-03-30 17:30:31 -0400

    Initial Commit

---
+++ War-Card-Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War-Card-Game
+//
+//  Created by Raghuram Duvvuri on 3/30/25.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
