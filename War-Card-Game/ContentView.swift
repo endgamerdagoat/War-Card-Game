620c6b2f282e907afd791469d13262e6eb1a7079
Raghuram Duvvuri <raghuramduvvuri12@gmail.com>
2025-03-30 17:35:41 -0400

    made initial ui

--- War-Card-Game/ContentView.swift
+++ War-Card-Game/ContentView.swift
@@ -9,13 +9,50 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Image("background-cloth")
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack {
+                Image("logo")
+                HStack {
+                    Spacer()
+                    Image("back")
+                    Spacer()
+                    Image("back")
+                    Spacer()
+                }.padding()
+                
+                Image("button")
+                VStack {
+                    HStack {
+                        Spacer()
+                        Text("Player")
+                            .font(.title2)
+                        Spacer()
+                        Text("CPU")
+                            .font(.title2)
+                        Spacer()
+                    }.padding()
+                    
+                    HStack {
+                        Spacer()
+                        Text("0")
+                            .font(.largeTitle)
+                        Spacer()
+                        Text("0")
+                            .font(.largeTitle)
+                        Spacer()
+                    }
+                    .padding()
+ 
+                }
+                .foregroundColor(Color.white)
+                .fontWeight(.bold)
+            }
+            
         }
-        .padding()
     }
 }
 
